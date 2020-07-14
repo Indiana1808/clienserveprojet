6c706160500753f040b64a5b13975b34d9a35651
Indiana1808 <jenitatindiana@yahoo.com>
2020-07-14 10:23:09 +0400

    v0.1

--- tcp/Client.c
+++ tcp/Client.c
@@ -1,51 +1,41 @@
 
 
-#include <stdio.h>
-#include <stdlib.h>
-
-#include <stdio.h>
-#include <stdlib.h>
-
-#include <sys/types.h>
-#include <sys/socket.h>
-
+#include <arpa/inet.h>
 #include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
 #include <unistd.h>
-
-#define PORT 9002
-
-int main(int argc, char ** argv)
-{
-
- char server_message[256] = "Voila notre message du server !";
-        int server_socket;
- int client_socket;
-
-
- server_socket= socket( AF_INET, SOCK_STREAM, 0);
-
-        struct  sockaddr_in  server_address;
-
-        server_address.sin_family = AF_INET;
-        server_address.sin_port = htons (PORT); 
-        server_address.sin_addr.s_addr = INADDR_ANY;
-
-
- bind(server_socket, (struct sockaddr*) &server_address, sizeof(server_address));
-
-
- listen (server_socket, 5); 
-
- client_socket = accept (server_socket, NULL, NULL );
-
-
-
-
- send(client_socket, server_message, sizeof(server_message), 0);
-
-
- close(server_socket);
-
-
-return EXIT_SUCCESS;
+#include <string.h>
+#define PORT 4000
+main() {
+   struct sockaddr_in address;
+   int my_socket = 0, valread;
+   struct sockaddr_in server_address;
+   char str[100];
+   int l;
+   printf("message: ");
+   fgets(str, 100, stdin); 
+   char buffer[1024] = { 0 }; /
+   if ((my_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+      printf("\nUnable to create Socket \n");
+      return -1;
+   }
+   memset(&server_address, '0', sizeof(server_address));
+   server_address.sin_family = AF_INET;
+   server_address.sin_port = htons(PORT);
+   if (inet_pton(AF_INET, "127.0.0.1", &server_address.sin_addr) <= 0) {
+      printf("\nThe address is not supported \n");
+      return -1;
+   }
+   // connect the socket
+   if (connect(my_socket, (struct sockaddr*)&server_address, sizeof(server_address)) < 0) {
+      printf("\nUnable to connect to the server \n");
+      return -1;
+   }
+   l = strlen(str);
+   send(my_socket, str, sizeof(str), 0); 
+   valread = read(my_socket, str, l); 
+   printf("%s\n", str);
 }
--- tcp/Serveur.c
+++ tcp/Serveur.c
@@ -1,58 +1,55 @@
+#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-#include <sys/types.h>
+#include <string.h>
 #include <sys/socket.h>
-
-#include <netinet/in.h>
 #include <unistd.h>
-
-#define PORT 9002
-
-int main(int argc, char ** argv)
-
-{
- int network_socket;
- int connection_status;
-
- 
- network_socket= socket( AF_INET, SOCK_STREAM, 0);
-
- 
-
- struct  sockaddr_in  server_address;
-
- server_address.sin_family = AF_INET;
- server_address.sin_port = htons (PORT); 
- server_address.sin_addr.s_addr = INADDR_ANY;
-
- connection_status = connect(network_socket, (struct sockaddr *) &server_address, sizeof (server_address)); 
-
- if (connection_status == -1)
- {
- 
- return EXIT_FAILURE;
- }
-
- char server_response[256];
-
- recv(network_socket, &server_response, sizeof(server_response), 0);
-
- printf("La reponse du serveur est : %s\n",server_response);
-
-
-
- close(network_socket);
-
- 
- return EXIT_SUCCESS;
-
-
+#include <string.h>
+#define PORT 4000
+char *strrev(char *str){
+   char *p1, *p2;
+   if (! str || ! *str)
+      return str;
+   for (p1 = str, p2 = str + strlen(str) - 1; p2 > p1; ++p1, --p2){
+      *p1 ^= *p2;
+      *p2 ^= *p1;
+      *p1 ^= *p2;
+   }
+   return str;
 }
-
-
-
-
-
-
-
+main() {
+   int server_fd, new_socket, valread;
+   struct sockaddr_in address;
+   char str[100];
+   int addrlen = sizeof(address);
+   char buffer[1024] = { 0 };
+   int i, j, temp;
+   int l;
+   char* message = "bonjour"; 
+   if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+      perror("Socket connection failed");
+      exit(EXIT_FAILURE);
+   }
+   address.sin_family = AF_INET;
+   address.sin_addr.s_addr = INADDR_ANY;
+   address.sin_port = htons(PORT);
+   if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) { 
+      perror("Unable to bind with the socket 4000");
+      exit(EXIT_FAILURE);
+   }
+   if (listen(server_fd, 3) < 0) {
+      perror("Listning.....");
+      exit(EXIT_FAILURE);
+   }
+   if ((new_socket = accept(server_fd, (struct sockaddr*)&address,(socklen_t*)&addrlen)) < 0) {
+      perror("Accept");
+      exit(EXIT_FAILURE);
+   }
+  
+   valread = read(new_socket, str, sizeof(str));
+   l = strlen(str);
+   printf("\n message client :%s\n", str);
+   strrev(str); //reverse the string
+   send(new_socket, str, sizeof(str), 0);
+   printf("\nrecevoir et envoyer \n");
+}
